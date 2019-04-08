9be3ddae4146ded6f7169dc92f09dd557eedbf49
Minsu, Kim <his4000@hanmail.net>
2019-04-08 23:21:50 +0900

    Change silago_dpu_2_max

--- matlab_lib/silago_dpu_2_max.m
+++ matlab_lib/silago_dpu_2_max.m
@@ -1,3 +1,4 @@
-function o = silago_dpu_2_max(input0,input1)
-        o = max(input0,input1);
+function [t0, t1] = silago_dpu_2_max(input0,input1)
+        t0 = max(input0);
+        t1 = max(input1);
 end
--- matlab_lib/silago_dpu_3_max.m
+++
@@ -1,3 +0,0 @@
-function o = silago_dpu_3_max(input0, input1, input2)
-        o = max(input0, input1, input2);
-end
